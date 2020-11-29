3537cc9bcc6fb3e0e3a5bb530c37889e0f5e8794
Ryan Rodriguez <ryanrod@vt.edu>
2020-11-29 01:06:38 -0500

    automate uv re-mapping with make.py

--- make.py
+++ make.py
@@ -2,6 +2,8 @@
 from shutil import copyfile
 import os
 import pathlib
+import sys
+import subprocess
 
 dirname = os.path.dirname(__file__)
 
@@ -15,42 +17,65 @@ textures = ["_a", "_d", "_p", "_r"]
 
 dist_dir = "dist/"
 
-#filename1 = os.path.join(dirname, 'Mesh/2L/Realistic Highway 2L.obj')
-#filename2 = os.path.join(dirname, 'dist/2L/Realistic Highway 2L.obj')
-#copyfile(filename1, filename2)
+def impord():
+#
+# Import and convert new mesh files
+#
+  subprocess.run(["blender", "--background", "--python", "scripts/blender_uv_remap.py",
+  "--", "import/2L/highway_bridge.obj", "mesh/2L/highway_bridge.obj"])
 
-# List mesh files
-for size in sizes:
-  output_dir = dist_dir + size + "/"
-  pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-  for elevation in elevations:
-    for mtype in mtypes:
-      for detail in details:
-        input_file = "Mesh/" + size + "/" + name + " " + size + elevation + mtype + ".obj"
-        output_file =  output_dir + name + " " + size + elevation + mtype + detail + ".obj"
+def dist():
+#
+# Make copies of files and layout in dist format
+#
 
-        try: 
-          copyfile(input_file, output_file)
-          print("Done: %-50s  ->  %10s" % (input_file, output_file))
-        except FileNotFoundError:
-          print("Warning: Input file %s not found!" % input_file)
+  # List mesh files
+  for size in sizes:
+    output_dir = dist_dir + size + "/"
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-# List textures
-for size in sizes:
-  for elevation in elevations:
-    for mtype in mtypes:
-      for detail in details:
-        for texture in textures:
-          if mtype == "_node" and texture == "_a":
-            # No alpha texture for nodes
-            continue
+    for elevation in elevations:
+      for mtype in mtypes:
+        for detail in details:
+          input_file = "Mesh/" + size + "/" + name + " " + size + elevation + mtype + ".obj"
+          output_file =  output_dir + name + " " + size + elevation + mtype + detail + ".obj"
 
-          input_file = "Textures/" + name + mtype + detail + texture + ".png"
-          output_file = output_dir + name + " " + size + elevation + mtype + detail + texture + ".png"
-          
           try: 
             copyfile(input_file, output_file)
             print("Done: %-50s  ->  %10s" % (input_file, output_file))
           except FileNotFoundError:
             print("Warning: Input file %s not found!" % input_file)
+
+  # List textures
+  for size in sizes:
+    for elevation in elevations:
+      for mtype in mtypes:
+        for detail in details:
+          for texture in textures:
+            if mtype == "_node" and texture == "_a":
+              # No alpha texture for nodes
+              continue
+
+            input_file = "Textures/" + name + mtype + detail + texture + ".png"
+            output_file = output_dir + name + " " + size + elevation + mtype + detail + texture + ".png"
+            
+            try: 
+              copyfile(input_file, output_file)
+              print("Done: %-50s  ->  %10s" % (input_file, output_file))
+            except FileNotFoundError:
+              print("Warning: Input file %s not found!" % input_file)
+
+
+
+if len(sys.argv) != 2:
+  print("Usage: make.py <goal>")
+  exit(0)
+
+goal = sys.argv[1]
+if goal == "import":
+  impord()
+elif goal == "dist":
+  dist()
+else:
+  print('unknown goal: %s' % goal)
--- scripts/blender_uv_remap.py
+++ scripts/blender_uv_remap.py
@@ -1,6 +1,7 @@
 import bpy
 import bmesh
 import sys
+import os
 
 # Read arguments
 argv = sys.argv
@@ -8,45 +9,70 @@ argv = argv[argv.index("--") + 1:]  # get all args after "--"
 
 if len(argv) != 2:
   print(len(argv))
-  print("Usage: blender --background --python <script.py> -- input_file output_file")
+  print("Usage: blender --background --python <script.py> -- input_dir output_dir")
   exit(0)
 
-file_in = argv[0]
-file_out = argv[1]
+def convert(input_file, output_file):
 
-# Load a Wavefront OBJ File
-bpy.ops.wm.read_factory_settings(use_empty=True)
-bpy.ops.import_scene.obj(filepath=file_in, filter_glob="*.obj;*.mtl", 
-use_edges=True, use_smooth_groups=True, use_split_objects=True, use_split_groups=True,
-use_groups_as_vgroups=False, use_image_search=True, split_mode='ON',
-axis_forward='-Z', axis_up='Y')
+  # Load a Wavefront OBJ File
+  bpy.ops.wm.read_factory_settings(use_empty=True)
+  bpy.ops.import_scene.obj(filepath=input_file, filter_glob="*.obj;*.mtl", 
+  use_edges=True, use_smooth_groups=True, use_split_objects=True, use_split_groups=True,
+  use_groups_as_vgroups=False, use_image_search=True, split_mode='ON',
+  axis_forward='-Z', axis_up='Y')
 
-#-------------------------------------------------
+  #-------------------------------------------------
+  #import bpy
+  #import bmesh
+  #import sys
 
-# Select object at index 0 (should be imported model)
-ob = bpy.data.objects[0]
+  # Select object at index 0 (should be imported model)
+  ob = bpy.data.objects[0]
 
-# Find faces that contain edges of length 4.x
-mesh = ob.data
-bm = bmesh.new()
-bm.from_mesh(mesh)
-uv_layer = bm.loops.layers.uv.verify()
+  # Find faces that contain edges of length 3.74
+  mesh = ob.data
+  bm = bmesh.new()
+  bm.from_mesh(mesh)
+  uv_layer = bm.loops.layers.uv.verify()
+  modified_faces = 0
 
-for face in bm.faces:
-    #for attr in dir(face):
-    #    print("face.%s = %r" % (attr, getattr(face, attr)))
-    for edge in face.edges:
-        if edge.calc_length() > 4 and edge.calc_length() < 5:
-            print(edge.calc_length())
-            
-            # Shift uv map
-            for loop in face.loops:
-                print(loop[uv_layer].uv, end='')
-                print(' -> ', end='')
-                loop[uv_layer].uv.x += 0.255
-                print(loop[uv_layer].uv)
-            break
+  for face in bm.faces:
+      #for attr in dir(face):
+      #    print("face.%s = %r" % (attr, getattr(face, attr)))
+      for edge in face.edges:
+          if edge.calc_length() > 3.7 and edge.calc_length() < 3.8:
+              #print(edge.calc_length())
+              modified_faces += 1
 
-# Write the mesh back and export
-bm.to_mesh(mesh)
-bpy.ops.export_scene.obj(filepath=file_out, use_materials=False)
+              # Shift uv map
+              for loop in face.loops:
+                  #print(loop[uv_layer].uv, end='')
+                  #print(' -> ', end='')
+                  loop[uv_layer].uv.x += 0.255
+                  #print(loop[uv_layer].uv)
+              break
+
+  print('------------------------')
+  print('UV shifted %i faces' % modified_faces)
+  print('------------------------')
+
+  # Write the mesh back
+  bm.to_mesh(mesh)
+
+  #-------------------------------------------------
+
+  # Export
+  bpy.ops.export_scene.obj(filepath=output_file, use_materials=False)
+
+
+dir_in = argv[0]
+dir_out = argv[1]
+
+directory = os.fsencode(dir_in)
+    
+for file in os.listdir(directory):
+     filename = os.fsdecode(file)
+     if filename.endswith(".obj"): 
+        convert(os.path.join(dir_in, filename), os.path.join(dir_out, filename))
+     else:
+         continue
